7c0919017f21d2c468d446360887f2e5d35f4188
Carlos Aliaga <carlosaligom@gmai.com>
2023-04-27 19:00:33 +0200

    Refactor Info test expect

--- src/components/Info/Info.test.tsx
+++ src/components/Info/Info.test.tsx
@@ -4,10 +4,12 @@ import Info from "./Info";
 describe("Given an Info component", () => {
   describe("When its rendered", () => {
     test("Then it should show 'Calling...' text on it", () => {
-      const expectedText = "Calling...";
+      const text = "Calling...";
       render(<Info />);
 
-      expect(screen.getByText(expectedText)).toBeInTheDocument();
+      const expectedText = screen.getByText(text);
+
+      expect(expectedText).toBeInTheDocument();
     });
   });
 });
